1fccfcb1feebc66664143f43f8ff672da62faa34
KhaledAbdellaty <kabdellaty@gmail.com>
2023-09-20 01:53:15 -0700

    monty-1.4

--- monty_functions2.c
+++ monty_functions2.c
@@ -9,14 +9,14 @@ void swap(stack_t **stack, unsigned int line_number)
 {
 	int temp;
 
-	if (*stack == NULL || (*stack)->prev == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	temp = (*stack)->n;
-	(*stack)->n = (*stack)->prev->n;
-	(*stack)->prev->n = temp;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 }
 
 /**
@@ -26,11 +26,11 @@ void swap(stack_t **stack, unsigned int line_number)
  */
 void add(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL || (*stack)->prev == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	(*stack)->prev->n += (*stack)->n;
+	(*stack)->next->n += (*stack)->n;
 	pop(stack, line_number);
 }

f7db76e303b0d7931220d021686a905eda734797
KhaledAbdellaty <kabdellaty@gmail.com>
2023-09-20 01:18:48 -0700

    monty-1.1

--- monty.h
+++ monty.h
@@ -41,7 +41,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, int value);
-void exec_push(const char *token, stack_t **stack, unsigned int line_number)
+void exec_push(const char *token, stack_t **stack, unsigned int line_number);
 void exec_instractions(const char *token, stack_t **stack,
-
+		instruction_t instructions[], unsigned int line_number);
 #endif
